a362e1b857e3fa8226f55f7960ea7afd038bce8f
julio.softbuilder <julio@softbuilder.com.br>
2023-03-03 00:03:51 -0300

    feat: ajust responsize

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,8 +12,8 @@ function Home() {
                 <div className='mb-3 text-3xl'>
                     <span>Julio Fonseca</span>
                 </div>
-                <div className='text-base mb-5' style={{ fontWeight: '300' }}>
-                    <span>21 • Frontend Enginer at <a href={'https://www.linkedin.com/in/ojuliofonseca/'} target={'_blank'}> <span className='hover:text-blue-300'>Softbuilder</span></a>• Ceará, Brazil</span>
+                <div className='text-base mb-5 text-center' style={{ fontWeight: '300' }}>
+                    <span>21 • Frontend Enginer at <a href={'https://www.softbuilder.com.br/web/'} target={'_blank'}> <span className='hover:text-blue-300'>Softbuilder</span></a>• Ceará, Brazil</span>
                 </div>
                 <div className='flex gap-5'>
                     <a href={'https://www.linkedin.com/in/ojuliofonseca/'} target={'_blank'}>
@@ -27,7 +27,7 @@ function Home() {
                     </a>
                 </div>
             </div>
-            <div className='flex gap-5 justify-end items-end max-h-screen text-white z-10 absolute bottom-5'>
+            <div className='flex gap-5 justify-end items-end max-h-screen text-white z-10 absolute bottom-2'>
                 <a href='mailto:juliocezar5716@gmail.com?Subject=Hey' className='hover:text-blue-300'> hire me </a>
                 <a href={resumePDF} target='_blank' className='hover:text-blue-300'> Resume </a>
             </div>
