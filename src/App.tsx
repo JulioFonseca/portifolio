54d3fd0d46eab7c69f3a763ade87ba117fb56ca7
julio.softbuilder <julio@softbuilder.com.br>
2023-03-05 17:25:26 -0300

    ajust speed

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ function App() {
                 default: "bounce",
               },
               random: false,
-              speed: 6,
+              speed: 1,
               straight: false,
             },
             number: {
